b704299206dfe439956f639b2371757d21c20bcc
OTM-07F <s1100608@sendai-nct.jp>
2015-02-22 04:04:16 +0900

    管理ページでログインしているか判定して動作するようにした

--- 07/manage.php
+++ 07/manage.php
@@ -10,73 +10,79 @@
 <?php
 	require_once '../lib/MySQL.php';		//接続は共通のクラスを使う
 	require_once 'header.php';
-	
-	global $QUESTION;
-	global $POSTED;
-	$cls 	= new MySQL();
-	$con	= $cls->mysqli_connect();
-	
-	if(!$con){
-		exit('データベースに接続できませんでした。');
-	}
-	$result = mysqli_query($con,'SET NAMES utf8');
-	if(!$result){
-			exit('文字コードを指定できませんでした。');
-	}
+	session_start();
+	if(isset($_SESSION["adminlogin"]) and $_SESSION["adminlogin"] == 1){
+		global $QUESTION;
+		global $POSTED;
+		$cls 	= new MySQL();
+		$con	= $cls->mysqli_connect();
+		
+		if(!$con){
+			exit('データベースに接続できませんでした。');
+		}
+		$result = mysqli_query($con,'SET NAMES utf8');
+		if(!$result){
+				exit('文字コードを指定できませんでした。');
+		}
 
-	//承認処理
-	if(isset($_POST['key']) and $_POST['key'] == 'utz'){
-		if(isset($_GET['num']) and isset($_POST['certify'])){
-			if($_POST['certify'] == '承認'){
-				$query	= 'SELECT * FROM '.$POSTED.' WHERE num ='.$_GET['num'];
-				$result	= mysqli_query($con,$query);
-				$data 	= mysqli_fetch_array($result);
-				$query	= 'INSERT INTO '.$QUESTION."(ryakusho,answer,fake1,fake2,fake3,name) VALUES('".$data['ryakusho']."','".$data['answer']."','".$data['fake1']."','".$data['fake2']."','".$data['fake3']."','".$data['name']."')";
+		//承認処理
+		if(isset($_POST['key']) and $_POST['key'] == 'utz'){
+			if(isset($_GET['num']) and isset($_POST['certify'])){
+				if($_POST['certify'] == '承認'){
+					$query	= 'SELECT * FROM '.$POSTED.' WHERE num ='.$_GET['num'];
+					$result	= mysqli_query($con,$query);
+					$data 	= mysqli_fetch_array($result);
+					$query	= 'INSERT INTO '.$QUESTION."(ryakusho,answer,fake1,fake2,fake3,name) VALUES('".$data['ryakusho']."','".$data['answer']."','".$data['fake1']."','".$data['fake2']."','".$data['fake3']."','".$data['name']."')";
+					$result	= mysqli_query($con,$query);
+					if (!$result) {
+		    				exit('データベースへの登録に失敗しました。');
+					}else{
+						echo "問題データの登録が完了しました。<br>\n";
+					}
+				}
+				else if($_POST['certify'] == '非承認'){
+					echo "問題を非承認とし、データベース上から削除しました。<br>\n";
+				}
+				$query	= 'DELETE FROM '.$POSTED.' WHERE num = '.$_GET['num'];
 				$result	= mysqli_query($con,$query);
 				if (!$result) {
-	    				exit('データベースへの登録に失敗しました。');
-				}else{
-					echo "問題データの登録が完了しました。<br>\n";
+		    			exit('データベースからの削除に失敗しました。');
 				}
 			}
-			else if($_POST['certify'] == '非承認'){
-				echo "問題を非承認とし、データベース上から削除しました。<br>\n";
-			}
-			$query	= 'DELETE FROM '.$POSTED.' WHERE num = '.$_GET['num'];
-			$result	= mysqli_query($con,$query);
-			if (!$result) {
-	    			exit('データベースからの削除に失敗しました。');
-			}
+		}else if(isset($_POST['key'])){
+			echo "パスワードが違います。<br>\n";
 		}
-	}else if(isset($_POST['key'])){
-		echo "パスワードが違います。<br>\n";
-	}
 
-	//投稿された問題とその承認フォーム
-	$query	= 'SELECT * FROM '.$POSTED;
-	$result	= mysqli_query($con,$query);
-	echo "<div>\n";
-	while ($data = mysqli_fetch_array($result)) {
-		if(empty($data['name'])){
-			echo '[No.'. $data['num'] . "] 無記名<br>\n";
-		}else{
-			echo '[No.'. $data['num'] . '] 出題者：' . htmlspecialchars($data['name'],ENT_QUOTES) . "<br>\n";
+		//投稿された問題とその承認フォーム
+		$query	= 'SELECT * FROM '.$POSTED;
+		$result	= mysqli_query($con,$query);
+		echo "<div>\n";
+		while ($data = mysqli_fetch_array($result)) {
+			if(empty($data['name'])){
+				echo '[No.'. $data['num'] . "] 無記名<br>\n";
+			}else{
+				echo '[No.'. $data['num'] . '] 出題者：' . htmlspecialchars($data['name'],ENT_QUOTES) . "<br>\n";
+			}
+			echo '問題とする略称：'.$data['ryakusho'] ."<br>\n";
+			echo "<br>\n";
+			echo 'その問題の正答：'.$data['answer']."<br>\n";
+			echo 'フェイク候補１：'.$data['fake1']."<br>\n";
+			echo 'フェイク候補２：'.$data['fake2']."<br>\n";
+			echo 'フェイク候補３：'.$data['fake3']."<br>\n";
+			echo '<form action="manage.php?num='.$data['num'].'" method="post">'."<br>\n";
+			echo 'パスワード　　：<input type="password" name="key">'."\n";
+			echo '<input type="submit" name="certify" value="承認">　<input type="submit" name="certify" value="非承認">'."<br>\n";
+			
 		}
-		echo '問題とする略称：'.$data['ryakusho'] ."<br>\n";
-		echo "<br>\n";
-		echo 'その問題の正答：'.$data['answer']."<br>\n";
-		echo 'フェイク候補１：'.$data['fake1']."<br>\n";
-		echo 'フェイク候補２：'.$data['fake2']."<br>\n";
-		echo 'フェイク候補３：'.$data['fake3']."<br>\n";
-		echo '<form action="manage.php?num='.$data['num'].'" method="post">'."<br>\n";
-		echo 'パスワード　　：<input type="password" name="key">'."\n";
-		echo '<input type="submit" name="certify" value="承認">　<input type="submit" name="certify" value="非承認">'."<br>\n";
-		
-	}
-	echo "</div>\n";
-	$con = mysqli_close($con);
-	if(!$con){
-		exit('データベースとの接続を閉じられませんでした。');
+		echo "</div>\n";
+		$con = mysqli_close($con);
+		if(!$con){
+			exit('データベースとの接続を閉じられませんでした。');
+		}
+	}else{
+		echo "<dl>\n";
+		echo '<dd><font color="#FF0000">このページを見るためには管理者権限が必要です。</font></dd>'."\n";
+		echo "</dl>\n";
 	}
 ?>
 <br><br>
